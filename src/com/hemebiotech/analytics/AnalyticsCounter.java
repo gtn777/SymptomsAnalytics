d569869f7b02d72fcd0be597e4f80176d984c197
gtn777 <gtn777@orange.fr>
2021-11-12 05:19:29 +0100

    first operative version

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,39 +1,51 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	
-	private static int rashCount = 0;		
-	private static int pupilCount = 0;		
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	private static String inputFilePath = "symptoms.txt";
+	private static String outputFilePath = "results.out";
 
-		while (line != null) {
-			if (line.equals("headache")) {
-				headacheCount++;
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("dialated pupils")) {
-				pupilCount++;
-			}
+	public static void main(String args[]) {
 
-			line = reader.readLine();	// get another symptom
+		// - 1 - extract data from input file with provided interface and class
+		ISymptomReader newRead = new ReadSymptomDataFromFile(inputFilePath); // path: symptoms.txt
+		List<String> rawDataList = newRead.getSymptoms();
+
+		// - 2 - Analyze data and provide enhanced listing
+		// Set sortedMap map as a tree map
+		Map<String, Integer> sortedMap = new TreeMap<String, Integer>();
+		// For each symptom in the raw list,
+		// If symptom is ever present inside sortedList, his occurrence is incremented
+		// Else a new symptom key is created in the map
+		for (String symptom : rawDataList) {
+			if (sortedMap.containsKey(symptom)) {
+				sortedMap.replace(symptom, sortedMap.get(symptom) + 1);
+			} else {
+				sortedMap.put(symptom, 1);
+			}
 		}
-		reader.close();
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("results.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+		// - 3 - Generate output:
+		if ((outputFilePath != null) && (!sortedMap.isEmpty())) {
+			try {
+				FileWriter writer = new FileWriter(outputFilePath);
+				System.out.println();
+				Set<String> symptoms = sortedMap.keySet();
+				for (String symptom : symptoms) {
+					writer.write(symptom + ": " + sortedMap.get(symptom) + "\n");
+				}
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("output file or entry data is not available");
+		}
 	}
 }
